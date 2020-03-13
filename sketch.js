f01e83f2f711fb4559ee5f9cd45b99ac409f5fb3
vivaan teotia <vivaan.teotia@gmail.com>
2020-03-13 16:53:08 +0530

    a commit

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,object;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +35,7 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    object=new box(160,310,70,70);
 
 }
 
